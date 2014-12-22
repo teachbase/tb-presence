c6d93847f462f23a1b6e38a072e91789cced421a
palkan <dementiev@webils.ru>
2014-12-22 13:43:10 +0400

    fix message handling

--- apps/tb_perf/src/tb_perf_server.erl
+++ apps/tb_perf/src/tb_perf_server.erl
@@ -30,7 +30,7 @@ authorize(Client,_) -> {ok, Client}.
 
 handle_message(_,_) -> ok.
 
-handle_client_message(_Client, Message) -> gen_server:call(?SERVER, {handle_client_message, Message}).
+handle_client_message(_Client, {text, Message}) -> gen_server:call(?SERVER, {handle_client_message, Message}).
 
 client_disconnected(_) -> ok.
 
